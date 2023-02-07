846f9a05f377852e955bd45bf80300d089660fb7
my2734 <myb1910105@student.ctu.edu.vn>
2023-02-07 13:20:36 +0700

    Doi theme cho ung dung

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyShop',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Lato',
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.purple,
+        ).copyWith(
+          secondary: Colors.deepOrange,
+        ),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('MyShop'),
+        ),
+        body: const Center(
+          child: Text('Welcome to myShop123567898989'),
+        ),
+      ),
+    );
+  }
+}
